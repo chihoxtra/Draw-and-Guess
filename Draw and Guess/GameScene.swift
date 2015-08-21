66c522b9c8aa802ef4dbde2efab8ec7e8920f954
chihoxtra <samuelpun@gmail.com>
2015-08-21 21:44:21 +0800

    after resintalling

---
+++ Draw and Guess/GameScene.swift
@@ -0,0 +1,205 @@
+//
+//  GameScene.swift
+//  DrawAndGuess//
+//  Created by pun samuel on 11/8/15.
+//  Copyright (c) 2015 Samuel Pun. All rights reserved.
+//
+
+import SpriteKit
+import GameKit
+
+class GameScene: SKScene {
+    
+    // reference to view controller to access items (like images) there
+    weak var viewController: GameViewController!
+    
+    var gtracer:Int = 0
+    
+    var gTimer:Int = 0
+    
+    var firstTimeDraw:Bool = true
+    
+    var gBrush:brush = brush(c: brushColorOption.black)
+    
+    class brush {
+        
+        var colorOption: brushColorOption
+        var color = UIColor()
+        var size: Int = 8
+        
+        init(c: brushColorOption) {
+            colorOption = c
+            switch colorOption {
+            case brushColorOption.black:
+                color = UIColor(red: 0.122, green: 0.122, blue: 0.122, alpha: 1.00)
+            case brushColorOption.darkBlue:
+                color = UIColor(red: 0.125, green: 0.000, blue: 0.502, alpha: 1.00)
+            case brushColorOption.blue:
+                color = UIColor(red: 0.200, green: 0.000, blue: 0.867, alpha: 1.00)
+            case brushColorOption.lightBlue:
+                color = UIColor(red: 0.000, green: 0.765, blue: 1.000, alpha: 1.00)
+            case brushColorOption.darkGreen:
+                color = UIColor(red: 0.000, green: 0.529, blue: 0.255, alpha: 1.00)
+            case brushColorOption.green:
+                color = UIColor(red: 0.000, green: 0.855, blue: 0.086, alpha: 1.00)
+            case brushColorOption.yellow:
+                color = UIColor(red: 0.914, green: 0.976, blue: 0.000, alpha: 1.00)
+            case brushColorOption.orange:
+                color = UIColor(red: 1.000, green: 0.365, blue: 0.000, alpha: 1.00)
+            case brushColorOption.red:
+                color = UIColor(red: 1.000, green: 0.000, blue: 0.000, alpha: 1.00)
+            case brushColorOption.magenta:
+                color = UIColor(red: 1.000, green: 0.000, blue: 0.718, alpha: 1.00)
+            case brushColorOption.pink:
+                color = UIColor(red: 1.000, green: 0.600, blue: 0.678, alpha: 1.00)
+            case brushColorOption.burgendy:
+                color = UIColor(red: 0.702, green: 0.004, blue: 0.098, alpha: 1.00)
+            }
+        }
+
+    }
+
+    
+    enum brushColorOption: String {
+        case black
+        case darkBlue
+        case blue
+        case lightBlue
+        case darkGreen
+        case green
+        case yellow
+        case orange
+        case red
+        case magenta
+        case pink
+        case burgendy
+    }
+    
+    func changeBrushValueStringtoValue(str: String) {
+        /* pass the label of button and change it to enum value */
+        gBrush = brush(c: brushColorOption(rawValue: str)!)
+
+    }
+    
+    /* a temp array holding points created */
+    var pointArray: [CGPoint] = [CGPoint(x: 0, y: 0)]
+
+    
+    func addPointsToArray(point:CGPoint) {
+        
+        pointArray.append(point)
+        
+    }
+    
+
+    func drawLineWithTwoPoints(fromPoint: CGPoint, toPoint: CGPoint) {
+        
+        let mainImageView = self.viewController.mainImageView
+        
+         /* A temporal View to hold the graphics before presentation */
+        let tmpImgView: UIImageView = UIImageView()
+        tmpImgView.frame.size = mainImageView.frame.size
+        
+        let viewWidth = mainImageView.frame.size.width
+        let viewHeight = mainImageView.frame.size.height
+        
+        /* Create a bitmap base image to present to Image View*/
+        UIGraphicsBeginImageContext((viewController?.mainImageView.frame.size)!)
+        let context = UIGraphicsGetCurrentContext()
+        
+
+        tmpImgView.image?.drawInRect(CGRect(x: 0, y: 0, width:  viewWidth, height: viewHeight))
+        
+        /* Creation of the line's two coordinates*/
+        
+        CGContextMoveToPoint(context, fromPoint.x, fromPoint.y)
+        CGContextAddLineToPoint(context, toPoint.x, toPoint.y)
+        
+        let myCIColor = CIColor(color: gBrush.color)
+
+        CGContextSetLineCap(context, CGLineCap(rawValue: 5)!)
+        CGContextSetLineWidth(context, 5)
+        CGContextSetRGBStrokeColor(context, myCIColor.red, myCIColor.green, myCIColor.blue, myCIColor.alpha)
+        
+        
+        CGContextSetBlendMode(context, CGBlendMode(rawValue: 1)!)
+        
+        /* Draw la*/
+        CGContextStrokePath(context)
+        
+        /* Draw on top of what is existing now */
+        tmpImgView.image = UIGraphicsGetImageFromCurrentImageContext()
+        tmpImgView.alpha = 1.0
+        
+        UIGraphicsEndImageContext()
+        
+        
+        // Merge tempImageView into mainImageView
+        UIGraphicsBeginImageContext((mainImageView?.frame.size)!)
+        
+        mainImageView!.image?.drawInRect(CGRect(x: 0, y: 0, width: viewWidth, height: viewHeight), blendMode: CGBlendMode(rawValue: 1)!, alpha: 1.0)
+        
+        tmpImgView.image?.drawInRect(CGRect(x: 0, y: 0, width: viewWidth, height: viewHeight), blendMode: CGBlendMode(rawValue: 1)!, alpha: 1.0)
+        
+        mainImageView!.image = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        
+        tmpImgView.image = nil
+        
+    }
+    
+    
+    override func didMoveToView(view: SKView) {
+        /* initilzing basics of apps */
+        
+//        _ = NSTimer.scheduledTimerWithTimeInterval(1.0, target: self, selector: Selector("updateTimer"), userInfo: nil, repeats: true)
+    }
+    
+    
+    func updateTimer() {
+        self.viewController.timerLabel.text = String(gTimer++)
+    }
+    
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        
+        /* if some residue points from last touch is still here*/
+
+        if pointArray.count > 0 {
+            pointArray.removeAll()
+        }
+        
+    }
+    
+    override func touchesEnded(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        
+    }
+    
+    override func touchesMoved(touches: Set<UITouch>, withEvent event: UIEvent?) {
+       /* Called when a touch begins */
+       
+        for touch in touches {
+
+            let touchLocation = (touch as UITouch).locationInView(self.viewController?.mainImageView)
+
+            addPointsToArray(touchLocation)
+
+        }
+    }
+    
+    
+    func consumePointsWithTimer() {
+        
+        while pointArray.count > 1 {
+            drawLineWithTwoPoints(pointArray[0], toPoint: pointArray[1])
+            pointArray.removeAtIndex(0)
+        }
+        
+    }
+   
+    override func update(currentTime: CFTimeInterval) {
+        /* Called before each frame is rendered */
+        
+            consumePointsWithTimer()
+
+    }
+}
---
+++ Draw and Guess/GameViewController.swift
@@ -0,0 +1,197 @@
+//
+//  GameViewController.swift
+//  DrawAndGuess
+//
+//  Created by pun samuel on 11/8/15.
+//  Copyright (c) 2015 Samuel Pun. All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+import GameKit
+
+
+class GameViewController: UIViewController, GKGameCenterControllerDelegate {
+    
+
+    var currentScene:GameScene = GameScene()
+    var lastButtonClicked:UIButton = UIButton()
+
+    var gGameStarted = false
+    
+    var gcEnabled:Bool = false
+    
+    var defaultButtonVerticalPosition:CGFloat = 450.0
+    
+
+
+    @IBOutlet weak var timerLabel: UILabel!
+    
+    @IBOutlet weak var keywordLabel: UILabel!
+
+    
+    /*Creating a list of references to the color pencils */
+
+    @IBOutlet var black: UIButton!
+    
+    @IBAction func black(sender: AnyObject) {
+        switchPencilColor(sender as! UIButton)
+    }
+    
+    @IBAction func darkBlue(sender: AnyObject) {
+        switchPencilColor(sender as! UIButton)
+    }
+    
+    @IBAction func blue(sender: AnyObject) {
+        switchPencilColor(sender as! UIButton)
+    }
+    
+    @IBAction func lightBlue(sender: AnyObject) {
+        switchPencilColor(sender as! UIButton)
+    }
+
+    @IBAction func darkGreen(sender: AnyObject) {
+        switchPencilColor(sender as! UIButton)
+    }
+
+    @IBAction func green(sender: AnyObject) {
+        switchPencilColor(sender as! UIButton)
+    }
+    
+    @IBAction func yellow(sender: AnyObject) {
+        switchPencilColor(sender as! UIButton)
+    }
+    
+    @IBAction func orange(sender: AnyObject) {
+        switchPencilColor(sender as! UIButton)
+    }
+    
+    @IBAction func red(sender: AnyObject) {
+        switchPencilColor(sender as! UIButton)
+    }
+    
+    @IBAction func magenta(sender: AnyObject) {
+        switchPencilColor(sender as! UIButton)
+    }
+    
+    /* This is the main view controller*/
+    @IBOutlet weak var mainImageView: UIImageView!
+    
+    func switchPencilColor(button: UIButton) {
+        lastButtonClicked.frame.origin.y = defaultButtonVerticalPosition /* reset pencils to the original positions */
+        button.frame.origin.y = button.frame.origin.y - 50
+        lastButtonClicked = button
+        currentScene.changeBrushValueStringtoValue((button.titleLabel?.text)!)
+    }
+    
+    func authenticatePlayer() {
+        /* Game initial settings set up */
+        
+        GKLocalPlayer.localPlayer().authenticateHandler = {( gameCenterVC, gameCenterError) -> Void in
+            if gameCenterVC != nil {
+                //showAuthenticationDialogWhenReasonable: is an example method name. Create your own method that displays an authentication view when appropriate for your app.
+                //showAuthenticationDialogWhenReasonable(gameCenterVC!)
+                self.presentViewController(gameCenterVC!, animated: true, completion: { () -> Void in
+                    // no idea
+                })
+            } else if GKLocalPlayer.localPlayer().authenticated == true {
+                print("game center ok")
+            } else  {
+                print("game center not ok")
+            }
+            
+            if gameCenterError != nil
+            { print("Game Center error: \(gameCenterError)")}
+        }
+    }
+    
+    func matchMaking() {
+        let gMatch:GKMatchmaker = GKMatchmaker()
+        let gInvite:GKInvite = GKInvite()
+        
+        gMatch.matchForInvite(gInvite , completionHandler: { (gMatch , gInviteErr) -> Void in
+            
+            if gInviteErr != nil {
+                // error out
+                print("Game Center error: \(gInviteErr)")
+            }
+            
+            if gMatch != nil {
+                // success   
+        
+            }
+        })
+        
+    }
+    
+    override func viewDidLoad() {
+        /* viewDidLoad is a good place to create and initialize subviews you wish to add to your main view. It is also a good place to further customize your main view. It's also a good place to initialize data structures because any properties should have been set on the view controller by the time this is called. This typically only needs to be done once. */
+        super.viewDidLoad()
+        mainImageView.frame.size = self.view.frame.size
+        
+        authenticatePlayer()
+
+    }
+    
+    override func viewWillLayoutSubviews() {
+        /* viewWillLayoutSubviews is where you position and layout the subviews if needed. This will be called after rotations or other events results in the view controller's view being sized. This can happen many times in the lifetime of the view controller.  */
+        if gGameStarted == false {
+            gGameStarted = true
+            super.viewWillLayoutSubviews()
+            
+            if let scene = GameScene(fileNamed:"GameScene") {
+                // Configure the view.
+                let skView = self.view as! SKView
+                skView.showsFPS = true
+                skView.showsNodeCount = true
+                
+                /* Sprite Kit applies additional optimizations to improve rendering performance */
+                skView.ignoresSiblingOrder = true
+                
+                /* Set the scale mode to scale to fit the window */
+                scene.scaleMode = .AspectFill
+                
+                skView.presentScene(scene)
+                
+                /* pass a reference of viewController to the scene */
+                scene.viewController = self
+                
+                /* get a reference to the scene */
+                currentScene = scene
+                
+
+            }
+        }
+    
+    }
+
+    override func shouldAutorotate() -> Bool {
+        return true
+    }
+
+    override func supportedInterfaceOrientations() -> UIInterfaceOrientationMask {
+        if UIDevice.currentDevice().userInterfaceIdiom == .Phone {
+            return .AllButUpsideDown
+        } else {
+            return .All
+        }
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Release any cached data, images, etc that aren't in use.
+    }
+
+    override func prefersStatusBarHidden() -> Bool {
+        return true
+    }
+    
+    func gameCenterViewControllerDidFinish(gameCenterViewController: GKGameCenterViewController) {
+        gameCenterViewController.dismissViewControllerAnimated(true, completion: nil)
+    }
+
+
+    
+    
+}
+
